0b69eb358da997b780dbf0cfcc9826fd6822af3f
Emi Zanotti <64757475+EmiZanotti@users.noreply.github.com>
2025-05-04 22:00:20 -0300

    feat: Agregada entity cliente

---
+++ src/models/cliente.js
@@ -0,0 +1,32 @@
+class Cliente {
+	constructor(nombre, apellido, contacto) {
+		this._nombre = nombre;
+		this._apellido = apellido;
+		this._contacto = contacto;
+		this.reservas = {};
+	}
+	nombre() {
+		return this._nombre;
+	}
+
+	apellido() {
+		return this._apellido;
+	}
+
+	contacto() {
+		return this._contacto;
+	}
+
+
+	agregarReserva(reserva) {
+		const idReserva = crypto.randomUUID(); // Cambiar por ID de objeto cuando se cree la entidad reserva
+		this.reservas[idReserva] = reserva;
+		return idReserva;
+	}
+
+	getReserva(idReserva) {
+		return this.reservas[idReserva]
+	}
+}
+
+module.exports = Cliente;
